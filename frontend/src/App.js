35a71e82cdb1a5b5a3e1077dc8d8d46af86242f6
Minindu Madhawa <minidumadawa259@gmail.com>
2026-04-15 12:35:33 +0530

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
+import {Route, Routes} from "react-router";
+import Home from "./components/Home/Home";
 
 
-function App() {
+function App(){
   return (
     <div>
-      
+      <React.Fragment>
+        <Routes>
+          <Route path="/" element={<Home></Home>}/>
+        </Routes>
+      </React.Fragment>
     </div>
   );
 }
-
-export default App;
