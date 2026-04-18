63a36ec4a73261aec966e2fc04cde867552ca65c
vishmi0104 <it23637078@my.sliit.lk>
2026-04-19 00:39:49 +0530

    facilitis module c

--- backend/src/main/java/backend/model/TicketCategory.java
+++ backend/src/main/java/backend/model/TicketCategory.java
@@ -3,6 +3,10 @@ package backend.model;
 public enum TicketCategory {
     MAINTENANCE,
     IT_SUPPORT,
+    IT_EQUIPMENT,
+    FURNITURE,
+    ELECTRICAL,
+    PLUMBING,
     CLEANING,
     FACILITY,
     OTHER
